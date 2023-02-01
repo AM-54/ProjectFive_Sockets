675cb0108d81181f5775075d7acba04e9c41e7a7
AM-54 <ahmad.mustafa@maqsoodlabs.com>
2023-02-01 13:49:03 +0500

    FirstCommit

---
+++ index.js
@@ -0,0 +1,12 @@
+const WebSocket=require('ws');
+const wss=new WebSocket.Server({port:3000});
+
+wss.on("connection",ws=>{
+    console.log("New Client connected!");
+    ws.on("close",()=>{
+        console.log("Client has disconnected!");
+    });
+    ws.on("message",(data)=>{
+        console.log(`The data recieved is ${data}`);
+    })
+});
